5207373c0c08656152d65668863c1e28669f6413
Nicolo Lüscher <nicolo.luescher@students.bfh.ch>
2022-11-21 11:24:44 +0100

    added seuquelize and created first running example

---
+++ backend/database.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_SCHEMA || 'postgres',
+                                process.env.DB_USER || 'postgres',
+                                process.env.DB_PASSWORD || '',
+                                {
+                                    host: process.env.DB_HOST || 'localhost',
+                                    port: process.env.DB_PORT || 5432,
+                                    dialect: 'postgres',
+                                    dialectOptions: {
+                                        ssl: process.env.DB_SSL == "true"
+                                    }
+                                });
+const Test = sequelize.define('Test', {
+    testValue1: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    testValue2: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+})
+module.exports = {
+    sequelize: sequelize,
+    Test: Test
+};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../database')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  db.Test.findAll()
+    .then(data => {
+        res.status(200).send(JSON.stringify(data))
+    })
+    .catch(error => {
+      res.status(500).send(error)
+    });
 });
 
 module.exports = router;
